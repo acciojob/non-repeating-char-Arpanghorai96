792cd91114e86039f45ef99a26ab806d0a461a09
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-05-04 13:08:13 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0;i<str.length;i++)
+		{
+			let count=0;
+			for(let y=0;y<str.length;y++)
+				{
+					if(str[i]==str[y])
+						count++;
+				}
+			if (count<2) {
+				return str[i];
+			}
+		}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
